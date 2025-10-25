29c1006103684f029df424f2a57ffc1a18d7b802
Anastasia Iogova <iogo3143@stthomas.edu>
2025-10-25 10:24:00 -0500

    Installed express and sqlite3 into our project, and connected the database.

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -1,7 +1,6 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as url from 'node:url';
-
 import { default as express } from 'express';
 import { default as sqlite3 } from 'sqlite3';
 
@@ -13,6 +12,16 @@ const template = path.join(__dirname, 'templates');
 
 let app = express();
 app.use(express.static(root));
+
+// open the database file and keep it open for the lifetime of my server
+const db = new sqlite3.Database('./earthquakes.sqlite3', sqlite3.OPEN_READONLY, (err) => { 
+    if (err) {
+        console.log('Error connecting to database');
+    }
+    else {
+        console.log('Successfully connected to database');
+    }
+}); 
 
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
